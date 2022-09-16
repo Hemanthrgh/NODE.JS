11f89b3140dc8ee69267ec28417bb01fe9a9745b
Hemanth <hemanthrgh61693@gmail.com>
2022-09-16 18:55:50 +0530

    the beginning/CN

---
+++ CodingNinja/Module_NODE_JS/index.js
@@ -0,0 +1,4 @@
+//just store all the function module have in object formate
+const func=require("./module");
+console.log(func.add(3,5));
+console.log(func.mult(3,5));
---
+++ CodingNinja/Module_NODE_JS/module.js
@@ -0,0 +1,10 @@
+// MODULE is a Libriry of set of reusable functions
+// Simple or complex functionality organized in a single or multiple JavaScript files which can be reused throughout your Node.js application is called Module.
+// How Node.js modules are made available externally? module.exports
+// Which function is used to include modules and libraries in Node Js?require
+module.exports.add=(a,b)=>{
+    return a+b;
+}
+module.exports.mult=(a,b)=>{
+    return a*b;
+}
